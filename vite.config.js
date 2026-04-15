a54f6d4b1515976e43a5e171c7c8030a9cd24619
Erik Albecker <erik@albecker.eu>
2026-04-15 11:46:58 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
   plugins: [
